93c09bf0c5ae4fdeeac936db3ecc1f10414d750d
Helen Thi <ht5658@gmail.com>
2015-01-21 09:16:33 -0500

    math rspec assessment complete

--- spec/math_spec.rb
+++ spec/math_spec.rb
@@ -13,3 +13,35 @@ RSpec.describe '#whatever method I'm testing' do
 end
 
 =end
+
+
+RSpec.describe '#square' do
+  it 'should square the number passed into the argument' do expect(square(5)).to eq 25
+      expect(square(7)).to eq 49
+      expect(square(10)).to eq 100
+    end
+  end
+
+  RSpec.describe '#cube' do
+  it 'should cube the number passed into the argument' do expect(cube(3)).to eq 27
+      expect(cube(1)).to eq 1
+      expect(cube(10)).to eq 1000
+    end
+  end
+
+  RSpec.describe '#perimeter' do
+  it 'should calculate the perimeter according to the length of the side of a square passed into the argument' do
+      expect(perimeter(4)).to eq 16
+      expect(perimeter(8)).to eq 32
+      expect(perimeter(12)).to eq 48
+    end
+  end
+
+
+  RSpec.describe '#cube_surface_area' do
+  it 'should calculate the surface area of a cube according to the length of an edge passed into the argument' do
+      expect(cube_surface_area(2)).to eq 24
+      expect(cube_surface_area(10)).to eq 600
+      expect(cube_surface_area(8)).to eq 384
+    end
+  end
